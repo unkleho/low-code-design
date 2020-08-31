403a793ad781aecfdafb3a7cab15363c4f1134ce
Kaho Cheung <kaho@elefanttraks.com>
2020-08-31 15:58:27 +1000

    Add Icon component

--- components/DesignTools/DesignTools.tsx
+++ components/DesignTools/DesignTools.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Utils, traverse } from 'react-fiber-traverse';
 import axios from 'axios';
 
+import Icon from '../Icon';
+
 import { FiberNode } from '../../types';
 
 type Props = {
@@ -346,69 +348,5 @@ function canUseDOM() {
     window.document.createElement
   );
 }
-
-const Icon = ({ name }) => {
-  switch (name) {
-    case 'chevron-down':
-      return (
-        <svg
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="chevron-down w-5 h-5"
-        >
-          <path
-            fillRule="evenodd"
-            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          ></path>
-        </svg>
-      );
-    case 'chevron-up':
-      return (
-        <svg
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="chevron-up w-5 h-5"
-        >
-          <path
-            fillRule="evenodd"
-            d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
-            clipRule="evenodd"
-          ></path>
-        </svg>
-      );
-    case 'chevron-right':
-      return (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-            clipRule="evenodd"
-          />
-        </svg>
-      );
-    case 'code':
-      return (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="w-5 h-5"
-        >
-          <path
-            fillRule="evenodd"
-            d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
-      );
-    default:
-      return null;
-  }
-};
 
 export default DesignTools;
