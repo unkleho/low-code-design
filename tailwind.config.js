5fe02ee899c1780ddfb1d61858ddb093be54df74
Kaho Cheung <kaho@elefanttraks.com>
2020-12-26 15:08:20 +1100

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 // const { colors } = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ['./components/**/*.js', './pages/**/*.js'],
+  // purge: ['./components/**/*.js', './pages/**/*.js'],
   // theme: {
   //   extend: {
   //     fontSize: {
