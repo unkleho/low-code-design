d7dc7cb0e6992333a45a370a858706ba3f550c96
Kaho Cheung <kaho@elefanttraks.com>
2023-09-03 09:24:25 +1000

    Fix tailwind on prod?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 // const { colors } = require('tailwindcss/defaultTheme')
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+
+    // Or if using `src` directory:
+    // "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   purge: ['./components/**/*.js', './pages/**/*.js'],
   // theme: {
   //   extend: {
